2694638301d1fd0f619b9ec4b9b0bd1a69df7f62
kovarike <danilokovarike@gmail.com>
2024-10-04 16:25:54 -0300

    app #000

--- src/lib/login.c
+++ src/lib/login.c
@@ -3,15 +3,28 @@
 #include <stdlib.h>
 #include <sqlite3.h>
 #include <string.h>
-#include <uuid/uuid.h>
+// #include <uuid/uuid.h>
 #include "../util/login.h"
 #include "../util/global.h"
 #include "../util/db.h"
 
+#include <time.h>
+
+void generate_uuid(char *uuid) {
+    srand(time(NULL));
+    sprintf(uuid, "%08x-%04x-%04x-%04x-%04x%08x",
+        rand(), rand() % 0x10000, (rand() % 0x0fff) + 0x4000,
+        (rand() % 0x3fff) + 0x8000, rand(), rand());
+}
+
+
 int login(sqlite3 *db) {
-    uuid_t binuuid;
-    uuid_generate(binuuid);
-    uuid_unparse(binuuid, user->id);
+    char uuid[37];  // Tamanho do UUID em formato string
+    generate_uuid(uuid);
+    strcpy(user->id, uuid);
+    // uuid_t binuuid;
+    // uuid_generate(binuuid);
+    // uuid_unparse(binuuid, user->id);
 
     
     if (check_user_exists(db) != 0 ) {
