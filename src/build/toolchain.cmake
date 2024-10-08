f6726da363b31ae994a8e7cd4fe7a73e2fd61d72
Kovarike <danilokovarike@gmail.com>
2024-10-08 14:56:01 -0300

    cmake 028egfre45rgrffaewf

--- src/build/toolchain.cmake
+++ src/build/toolchain.cmake
@@ -1,16 +1,16 @@
+# Specify the cross compiler
 set(CMAKE_SYSTEM_NAME Windows)
-
-
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
+# Specify the toolchain file path
 set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
-set(CMAKE_INCLUDE_PATH ${CMAKE_INCLUDE_PATH} /usr/x86_64-w64-mingw32/include)
-set(CMAKE_LIBRARY_PATH ${CMAKE_LIBRARY_PATH} /usr/x86_64-w64-mingw32/lib)
 
-
+# Adjust the include directories
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+
+
