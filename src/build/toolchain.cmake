06f78ba9ca7ed92a83e40e2ca099691d1ba7bb38
Kovarike <danilokovarike@gmail.com>
2024-10-08 14:16:37 -0300

    cmake 027

--- src/build/toolchain.cmake
+++ src/build/toolchain.cmake
@@ -1,33 +1,9 @@
-# toolchain.cmake
-
-# Especificar o nome do sistema
 set(CMAKE_SYSTEM_NAME Windows)
-
-# Especificar o compilador cruzado
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
-set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-
-
-# Especificar o caminho da ferramenta
+set(CMAKE_C_COMPILER /usr/bin/x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER /usr/bin/x86_64-w64-mingw32-windres)
 set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
 
-# Ajustar os diretórios de inclusão
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
-
-set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
-
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ALWAYS)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ALWAYS)
-
