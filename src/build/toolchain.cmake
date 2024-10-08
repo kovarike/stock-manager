5381eae4c78a410bafc8457afdab9270d7b06240
Kovarike <danilokovarike@gmail.com>
2024-10-08 14:37:31 -0300

    cmake 028egfre45rgrffaewf

--- src/build/toolchain.cmake
+++ src/build/toolchain.cmake
@@ -1,4 +1,11 @@
 set(CMAKE_SYSTEM_NAME Windows)
+
+
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+
 set(CMAKE_C_COMPILER /usr/bin/x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER /usr/bin/x86_64-w64-mingw32-windres)
